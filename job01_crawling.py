c19246c90e7fdebcc18b68adc8a0ac6fc3371662
scolp <scolpig@gmail.com>
2022-12-07 10:45:19 +0900

    crawling 작업 지시

--- job01_crawling.py
+++ job01_crawling.py
@@ -1,14 +1,25 @@
-from selenium import webdriver
-import pandas as pd
-from selenium.common.exceptions import NoSuchElementException
-import time
 
-options = webdriver.ChromeOptions()
+# crawling 작업
 
-options.add_argument('lang=ko_KR')
-driver = webdriver.Chrome('./chromedriver.exe', options=options)
+# crawling은 각자 진행해보고 빨리 완성되는 코드로 연도를 나눠서 작업할게요.
+# 일단 2022년 개봉작만 클롤링 해주세요.
+# 나머지는 연도별로 나눠서 작업할게요.
+# 컬럼명은 ['titles', 'reviews']로 통일해주세요.
+# 파일명은 'reviews_{}.csv'.format(연도)'로 해주세요.
+# crawling 코드 완성되는대로 PR해주세요.
 
-url = 'https://movie.naver.com/movie/sdb/browsing/bmovie.naver?open=2022&page=1'
+
+# from selenium import webdriver
+# import pandas as pd
+# from selenium.common.exceptions import NoSuchElementException
+# import time
+#
+# options = webdriver.ChromeOptions()
+#
+# options.add_argument('lang=ko_KR')
+# driver = webdriver.Chrome('./chromedriver.exe', options=options)
+#
+# url = 'https://movie.naver.com/movie/sdb/browsing/bmovie.naver?open=2022&page=1'
 
 
 
