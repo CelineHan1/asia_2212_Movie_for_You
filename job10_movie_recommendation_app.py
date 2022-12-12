cc6943cfc2d70a90fa7029355ca8af6aa3b5e951
scolp <scolpig@gmail.com>
2022-12-12 12:06:45 +0900

    recommendation app 02

--- job10_movie_recommendation_app.py
+++ job10_movie_recommendation_app.py
@@ -34,15 +34,43 @@ class Exam(QWidget, form_window):
         self.line_edit.setCompleter(completer)
 
         self.combo_box.currentIndexChanged.connect(self.combobox_slot)
+        self.btn_recommend.clicked.connect(self.btn_slot)
 
-    def combobox_slot(self):
-
-        title = self.combo_box.currentText()
+    def recommendation_by_movie_title(self, title):
         movie_idx = self.df_reviews[self.df_reviews['titles'] == title].index[0]
         cosin_sim = linear_kernel(self.tfidf_matrix[movie_idx], self.tfidf_matrix)
         recommendation = self.getRecommendation(cosin_sim)
         recommendation = '\n'.join(list(recommendation[1:]))
         self.lbl_recommend.setText(recommendation)
+
+    def btn_slot(self):
+        key_word = self.line_edit.text()
+        if key_word in self.titles:
+            self.recommendation_by_movie_title(key_word)
+        elif key_word in list(self.embedding_model.wv.index_to_key):
+            embedding_model = Word2Vec.load('./models/word2vec_movie_review.model')
+            sim_word = embedding_model.wv.most_similar(key_word, topn=10)
+            words = [key_word]
+            for word, _ in sim_word:
+                words.append(word)
+            print(words)
+            sentence = []
+            count = 11
+            for word in words:
+                sentence = sentence + [word] * count
+                count -= 1
+            sentence = ' '.join(sentence)
+            print(sentence)
+            sentence_vec = self.tfidf.transform([sentence])
+            cosin_sim = linear_kernel(sentence_vec, self.tfidf_matrix)
+            recommendation = self.getRecommendation(cosin_sim)
+            recommendation = '\n'.join(list(recommendation[1:]))
+            self.lbl_recommend.setText(recommendation)
+
+    def combobox_slot(self):
+
+        title = self.combo_box.currentText()
+        self.recommendation_by_movie_title(title)
 
     def getRecommendation(self, cosin_sim):
         simScore = list(enumerate(cosin_sim[-1]))
